1e3f655e8ecd5f48b347e32779eae288fc29224e
Jairus Joer・Macbook Pro <jairus.joer@neuwaerts.de>
2020-09-21 14:37:01 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+    // Pass through assets
+    eleventyConfig.addPassthroughCopy('src/assets/data');
+    eleventyConfig.addPassthroughCopy('src/assets/images');
+    eleventyConfig.addPassthroughCopy('src/assets/js');
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist',
+            data: 'data',
+            includes: 'includes',
+            layouts: 'layouts',
+        },
+    };
+};
